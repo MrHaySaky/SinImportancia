8783d1d1b2ecb6d1ee80c387f6628d1120beba59
MrHaySaky <carlos_zrm@outlook.com>
2014-04-19 13:46:44 -0500

    bubble.java
    dew

---
+++ bubble.java
@@ -0,0 +1,21 @@
+public class bubble{
+
+double matriz, aux;
+boolean per;
+double len; 
+bubble (double[] m){
+this.matriz=m;
+
+}
+
+ordena(){
+per=true;
+aux=0;
+
+while(per&&){
+for(Double ax: matriz){
+
+}
+}
+
+}
